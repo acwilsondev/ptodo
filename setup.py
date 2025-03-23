053c553ffca3079bf7adfb832239fb26a772ab75
Aaron Wilson <acwilsoncs@gmail.com>
2025-03-23 11:37:50 -0600

    Bump version to 1.1.0
    New features:
    - Added default_list_count configuration option
    - Improved list command limit behavior

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ptodo",
-    version="1.0.0",
+    version="1.1.0",
     description="Plaintext Todo.txt CLI",
     author="Aaron Wilson",
     author_email="aaron@acwilson.dev",
